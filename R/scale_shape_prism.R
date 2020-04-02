c6a67a3216f89be4e1aaf9e82c2b1678621ea487
Charlotte Dawson <csdaw@outlook.com>
2020-04-02 15:16:09 +0100

    Include pch shape names

--- R/scale_shape_prism.R
+++ R/scale_shape_prism.R
@@ -1,7 +1,22 @@
 prism_shape_pal <- function() {
-  shapes <- c()
-  values <- c(16, 15, 17, 18, 21, 22, 24, 25, 23, 8, 3, 4, 13, 7)
-  out <- scales::manual_pal(shapes)
+  shapes <- c(
+    # standard shapes
+    "circle small", "square", "triangle", "diamond",
+    # filled shapes
+    "circle filled", "square filled", "triangle filled",
+    "triangle down filled", "diamond filled",
+    # other shapes,
+    "asterisk", "plus", "cross", "circle plus", "square cross"
+    )
+  values <- c(
+    # standard shapes
+    16, 15, 17, 18,
+    # filled shapes
+    21, 22, 24, 25, 23,
+    # other shapes
+    8, 3, 4, 10, 7
+    )
+  out <- scales::manual_pal(values)
   attr(out, "max_n") <- length(shapes)
   out
 }
@@ -9,6 +24,3 @@ prism_shape_pal <- function() {
 scale_shape_prism <- function(...) {
   discrete_scale("shape", "prism", prism_shape_pal(), ...)
 }
-
-
-
