c59183baa5ab5376570973b268cff2b0d4f8da58
Charlotte Dawson <csdaw@outlook.com>
2020-04-03 19:08:39 +0100

    Add minor ticks annotation function

---
+++ R/annotation_ticks.R
@@ -0,0 +1,175 @@
+# code from teunbrand's answer to https://stackoverflow.com/questions/58485334
+# which is itself a refactored version of hrbrmstr's annotation_ticks function.
+# https://github.com/hrbrmstr/ggalt/blob/master/R/annotation_ticks.r
+annotation_ticks <- function(sides = "b",
+                             scale = "identity",
+                             scaled = TRUE,
+                             ticklength = unit(0.1, "cm"),
+                             colour = "black",
+                             size = 0.5,
+                             linetype = 1,
+                             alpha = 1,
+                             color = NULL,
+                             ticks_per_base = NULL,
+                             ...) {
+  if (!is.null(color)) {
+    colour <- color
+  }
+
+  # check for invalid side
+  if (grepl("[^btlr]", sides)) {
+    stop(gsub("[btlr]", "", sides), " is not a valid side: b,t,l,r are valid")
+  }
+
+  # split sides to character vector
+  sides <- strsplit(sides, "")[[1]]
+
+  if (length(sides) != length(scale)) {
+    if (length(scale) == 1) {
+      scale <- rep(scale, length(sides))
+    } else {
+      stop("Number of scales does not match the number of sides")
+    }
+  }
+
+  base <- sapply(scale, function(x) switch(x, "identity" = 10, "log10" = 10, "log" = exp(1)), USE.NAMES = FALSE)
+
+  if (missing(ticks_per_base)) {
+    ticks_per_base <- base - 1
+  } else {
+    if ((length(sides) != length(ticks_per_base))) {
+      if (length(ticks_per_base) == 1) {
+        ticks_per_base <- rep(ticks_per_base, length(sides))
+      } else {
+        stop("Number of ticks_per_base does not match the number of sides")
+      }
+    }
+  }
+
+  delog <- scale %in% "identity"
+
+  layer(
+    data = data.frame(x = NA),
+    mapping = NULL,
+    stat = StatIdentity,
+    geom = GeomTicks,
+    position = PositionIdentity,
+    show.legend = FALSE,
+    inherit.aes = FALSE,
+    params = list(
+      base = base,
+      sides = sides,
+      scaled = scaled,
+      ticklength = ticklength,
+      colour = colour,
+      size = size,
+      linetype = linetype,
+      alpha = alpha,
+      ticks_per_base = ticks_per_base,
+      delog = delog,
+      ...
+    )
+  )
+}
+
+GeomTicks <- ggproto(
+  "GeomTicks", Geom,
+  extra_params = "",
+  handle_na = function(data, params) {
+    data
+  },
+
+  draw_panel = function(data,
+                        panel_scales,
+                        coord,
+                        base = c(10, 10),
+                        sides = c("b", "l"),
+                        scaled = TRUE,
+                        ticklength = unit(0.1, "cm"),
+                        ticks_per_base = base - 1,
+                        delog = c(x = TRUE, y = TRUE)) {
+    ticks <- list()
+
+    for (s in 1:length(sides)) {
+      if (grepl("[b|t]", sides[s])) {
+
+        xticks <- panel_scales$x.minor
+
+        # Make the grobs
+        if (grepl("b", sides[s])) {
+          ticks$x_b <- with(
+            data,
+            segmentsGrob(
+              x0 = unit(xticks, "npc"),
+              x1 = unit(xticks, "npc"),
+              y0 = unit(0, "npc"),
+              y1 = ticklength,
+              gp = gpar(
+                col = alpha(colour, alpha),
+                lty = linetype,
+                lwd = size * .pt
+              )
+            )
+          )
+        }
+        if (grepl("t", sides[s])) {
+          ticks$x_t <- with(
+            data,
+            segmentsGrob(
+              x0 = unit(xticks, "npc"),
+              x1 = unit(xticks, "npc"),
+              y0 = unit(1, "npc"),
+              y1 = unit(1, "npc") - ticklength,
+              gp = gpar(
+                col = alpha(colour, alpha),
+                lty = linetype,
+                lwd = size * .pt
+              )
+            )
+          )
+        }
+      }
+
+
+      if (grepl("[l|r]", sides[s])) {
+
+        yticks <- panel_scales$y.minor
+
+        # Make the grobs
+        if (grepl("l", sides[s])) {
+          ticks$y_l <- with(
+            data,
+            segmentsGrob(
+              y0 = unit(yticks, "npc"),
+              y1 = unit(yticks, "npc"),
+              x0 = unit(0, "npc"),
+              x1 = ticklength,
+              gp = gpar(
+                col = alpha(colour, alpha),
+                lty = linetype, lwd = size * .pt
+              )
+            )
+          )
+        }
+        if (grepl("r", sides[s])) {
+          ticks$y_r <- with(
+            data,
+            segmentsGrob(
+              y0 = unit(yticks, "npc"),
+              y1 = unit(yticks, "npc"),
+              x0 = unit(1, "npc"),
+              x1 = unit(1, "npc") - ticklength,
+              gp = gpar(
+                col = alpha(colour, alpha),
+                lty = linetype,
+                lwd = size * .pt
+              )
+            )
+          )
+        }
+      }
+    }
+    gTree(children = do.call("gList", ticks))
+  },
+  default_aes = aes(colour = "black", size = 0.5, linetype = 1, alpha = 1)
+)
