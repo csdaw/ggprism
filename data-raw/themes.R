0a31b546eb9071710a655d06f615aab243a31c6f
Charlotte Dawson <csdaw@outlook.com>
2020-05-02 14:50:54 +0100

    Extract theme colours from text files

---
+++ data-raw/themes.R
@@ -0,0 +1,34 @@
+# Extract Themes from Prism Colour Schemes
+#
+# Extract themes from Prism colour scheme .txt files. Text files were
+# generated from .clr files included with an installed version of Prism on
+# MacOS using a Swift script.
+#
+# See:
+#
+# - https://stackoverflow.com/questions/49379341
+#
+library(readr)
+library(dplyr)
+library(purrr)
+library(here)
+
+scheme_dir <- "data-raw/schemes"
+
+scheme_files <- dir(scheme_dir, pattern = "\\.txt$",
+                    full.names = TRUE, recursive = TRUE)
+
+process_prism_scheme <- function(path) {
+  theme <- read_delim(path, delim = " ",
+             col_names = c("name", "r", "g", "b", "value")) %>%
+    select(name, value) %>%
+    filter(!grepl("^dataSet", name))
+}
+
+
+themes <- map(scheme_files, process_prism_scheme)
+
+names(themes) <- tools::file_path_sans_ext(basename(scheme_files))
+
+cat(yaml::as.yaml(themes, column.major = FALSE),
+    file = here("data-raw", "themes.yml"))
