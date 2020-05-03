0ff1f1ad47adc3110c34e737ac7471cb1bd52d11
Charlotte Dawson <csdaw@outlook.com>
2020-05-03 14:43:34 +0100

    Add fill scale functions

---
+++ R/scale_fill_prism.R
@@ -0,0 +1,33 @@
+#' Prism fill palettes (discrete)
+#'
+#' Description.
+#'
+#' @param palette string. Palette name, see names(ggprism_data$fill_palettes)
+#' for valid palette names.
+#' @family fill
+#' @export
+#' @example inst/examples/ex-prism_fill_pal.R
+prism_fill_pal <- function(palette = "colors") {
+  allfills <- ggprism::ggprism_data$fill-palettes
+  if (!palette %in% names(allfills)) {
+    stop("The palette ", paste(palette), " does not exist.
+         See names(ggprism_data$fill_palettes) for valid palette names.")
+  }
+  values <- unname(allfills[[palette]])
+  f <- manual_pal(values)
+  attr(f, "max_n") <- length(values)
+  f
+}
+
+#' Prism fill scales (discrete)
+#'
+#' See \code{\link{prism_fill_pal}()} for details.
+#'
+#' @inheritParams prism_fill_pal
+#' @param ... Other arguments passed on to \funclink{discrete_scale}.
+#' @family fill prism
+#' @example inst/examples/ex-scale_fill_prism.R
+#' @export
+scale_fill_prism <- function(palette = "colors", ...) {
+  discrete_scale("fill", "prism", prism_fill_pal(palette = palette), ...)
+}
---
+++ inst/examples/ex-scale_fill_prism.R
@@ -0,0 +1,1 @@
+# need to write an example
