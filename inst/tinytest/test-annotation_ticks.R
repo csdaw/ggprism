1e92f6e38ca1c3d4b2f818b00be55fe2c82319e9
Charlotte Dawson <csdaw@outlook.com>
2020-11-05 14:00:58 +0000

    Test annotation ticks

---
+++ inst/tinytest/test-annotation_ticks.R
@@ -0,0 +1,59 @@
+#### Setup ---------------------------------------------------------------------
+
+## load libraries
+library(ggplot2)
+
+p <- ggplot(msleep, aes(bodywt, brainwt)) + geom_point(na.rm = TRUE)
+
+#### Tests ---------------------------------------------------------------------
+
+# test that the function with default arguments works
+g <- p + annotation_ticks()
+
+expect_equal(length(layer_grob(g)), 1)
+
+# test that the function recognises the sides argument
+g <- p + annotation_ticks(sides = "trbl")
+
+expect_equal(length(layer_grob(g)), 1)
+expect_equal(length(layer_grob(g, 2L)[[1]]$children), 4)
+
+expect_error(p + annotation_ticks(sides = "banana"))
+
+# test that the type argument works
+g1 <- p + annotation_ticks(type = "both")
+g2 <- p + annotation_ticks(type = "major")
+g3 <- p + annotation_ticks(type = "minor")
+
+expect_equal(length(layer_grob(g1)), 1)
+expect_equal(length(layer_grob(g2)), 1)
+expect_equal(length(layer_grob(g3)), 1)
+
+expect_equal(length(layer_grob(g1, 2L)[[1]]$children[[1]]$x0), 8)
+expect_equal(length(layer_grob(g2, 2L)[[1]]$children[[1]]$x0), 5)
+expect_equal(length(layer_grob(g3, 2L)[[1]]$children[[1]]$x0), 3)
+
+expect_error(p + annotation_ticks(type = "banana"))
+
+# test that ticks can go outside
+g <- p + annotation_ticks(outside = TRUE) +
+  coord_cartesian(clip = "off")
+
+expect_equal(length(layer_grob(g)), 1)
+
+ticks <- layer_grob(g, 2L)[[1]]$children[[1]]$y1
+lapply(
+  ticks,
+  function(x) expect_true(as.numeric(x) < 0)
+)
+
+# test that tick lengths can be set
+g <- p + annotation_ticks(
+  type = "both",
+  tick.length = unit(20, "pt"),
+  minor.length = unit(10, "pt")
+)
+
+expect_equal(length(layer_grob(g)), 1)
+expect_identical(g$layers[[2]]$geom_params$tick.length, unit(20, "pt"))
+expect_identical(g$layers[[2]]$geom_params$minor.length, unit(10, "pt"))
--- inst/tinytest/test-utils.R
+++ inst/tinytest/test-utils.R
@@ -1,4 +1,4 @@
 # test that ggplot2 internal function grabber works
-x <- .grab_ggplot_internals()
+x <- ggprism:::.grab_ggplot_internals()
 classes <- sapply(x, class)
 expect_equal(length(classes), 4)
